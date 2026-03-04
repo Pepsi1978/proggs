2c31b44746976645523aef221d692b6481989271
Pepsi1978 <barwandt@gmail.com>
2026-03-04 14:01:11 +0100

    Delete Claude Overlay Windows directory

--- Claude Overlay Windows/start_overlay_debug.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-python "%~dp0src\overlay_app.py"
-endlocal
--- Claude Overlay Windows/start_watcher.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-start "ClaudeOverlayWatcher" pythonw "%~dp0src\watcher.py"
-endlocal
