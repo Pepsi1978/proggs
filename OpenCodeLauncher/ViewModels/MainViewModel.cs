e04dd12f631d8d29980e003ce8745ad9d6c60641
barwa <barwandt@gmail.com>
2026-07-13 12:12:58 +0200

    Start profile editor maximized

--- OpenCodeLauncher/ViewModels/MainViewModel.cs
+++ OpenCodeLauncher/ViewModels/MainViewModel.cs
@@ -55,8 +55,8 @@ public sealed partial class MainViewModel : ObservableObject
         WorkDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "proggs");
         _ = CheckOpenCodeUpdateAsync();
 
-        var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "1.16.6";
-        Version = $"Version {version} (13.07.2026, 12:05 Uhr)";
+        var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "1.16.7";
+        Version = $"Version {version} (13.07.2026, 12:11 Uhr)";
     }
 
     public ObservableCollection<ModelGroupEntry> ModelGroups { get; } = new();
